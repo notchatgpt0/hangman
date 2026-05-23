f43dc73d930bb9fd34a1c1731f2ea7914c12e78e
notchatgpt <160557384+notchatgpt0@users.noreply.github.com>
2026-05-22 20:21:25 -0400

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -1,9 +1,9 @@
 import random
 # imports the random number selector
- 
+
 inputted_password = input("Enter in the password required to start the game: ")
 # ask for the alphanumeric input
- 
+
 has_letter = inputted_password.isalpha()
 has_digit = inputted_password.isdigit()
 has_symbol = False
@@ -11,7 +11,7 @@ for character in inputted_password:
     if character not in "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM":
         has_symbol = True
 # variables to check if the inputted password by the user will have a letter and a digit later
- 
+
 while has_letter or has_digit or has_symbol:
     print("Password must have at least one letter and one number. Try again.")
     inputted_password = input("Enter in the password required to start the game: ")
@@ -26,30 +26,30 @@ while has_letter or has_digit or has_symbol:
 wrong_letters = 0
 list_of_words = ["tree", "car", "nickel", "hangman", "penny", "apple", "battery", "rocket", "music", "eat", "football", "basketball", "baseball", "soccer", "softball", "tennis", "phone", "computer", "spray"]
 # the list of words that the program can choose from
- 
+
 selected_word = random.choice(list_of_words)
 # chooses a random word and puts gives it to the user for them to guess
- 
+
 correct_letters = 0
 # counts how many correct letters were guessed
- 
+
 length = len(selected_word)
 if length > 6:
     guesses = 12
 else:
     guesses = 8
 # code above sets everything to the default value at the start
- 
+
 print("Your word has " + str(length) + " letters, and you have " + str(guesses) + " guesses.")
 print("Begin guessing letters")
 # tells the player to start guessing letters
- 
+
 display = ["_"] * length
 # the display
- 
+
 print(" ".join(display))
 # pre prints so that the player knows how many letters are in the hidden word
- 
+
 while "_" in display and wrong_letters < guesses:
     user_input = input("Guess a letter: ").lower()
     if user_input in selected_word:
@@ -63,14 +63,14 @@ while "_" in display and wrong_letters < guesses:
         print("Incorrect guess! You have " + str(guesses - wrong_letters) + " guesses left!")
     print(" ".join(display))
 # updates the display to show where the correct letters are if the player guesses it, if the player guesses wrong the amount of guesses they have left decreases
- 
+
 if "_" not in display:
     print("You won!")
     # checks if the player has guessed all of the letters and revealed the word
 elif wrong_letters >= guesses:
     print("You've run out of guesses! The word was: " + selected_word)
     # if the player has ran out of guesses they will lose and be given the word
- 
+
 if correct_letters > 0:
     guess_ratio = round(float(guesses / correct_letters), 2)
     # default difficulty ratio value, difficult ratio is based on guesses divided by correct letters
